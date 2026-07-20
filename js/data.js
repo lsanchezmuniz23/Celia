07773674db8196f81b07dbc952b16eba235ca8ce
lsanchezmuniz23 <luciasanchezmuniz23@gmail.com>
2026-07-20 23:54:47 +0200

    Update data.js

--- js/data.js
+++ js/data.js
@@ -300,7 +300,7 @@ window.CELIA_DATA = {
     {
       "id": "paula",
       "symbol": "⚜",
-      "author": "Paulita",
+      "author": "Paula Donas",
       "title": "Holi Celiaa!! Soy Paula!",
       "preview": "Muchas, muchas felicidades, qué tía más chula!",
       "body": "Holi Celiaa!! Soy Paula! \nMuchas, muchas felicidades, qué tía más chula! Eres una persona súper icónica y tenemos mucha suerte de haberte conocido. Que sepas que en muchas cosas, eres nuestro referente. \nNos enseñas a no tener miedo a los cambios, a tener ilusión con proyectos muy tuyos y a no olvidarte de querer a los tuyos por muy a tope que estés. Y a ser súper disfrutonas a pesar de todo.\nY además es que eres una persona que es agradable tener a nuestro lado. Eres graciosa, valiente y estás loquita perdida, me encanta.\nTe estoy súper agradecida por todo lo que haces por nosotras, como animadora pero sobretodo como persona ❤️\nQué alegría conocerte y compartir camino contigo, \npor muchos años más compartiendo la locura que es la vida,\nPaulita"
@@ -308,7 +308,7 @@ window.CELIA_DATA = {
     {
       "id": "perfecta-celia",
       "symbol": "🫶🏼",
-      "author": "",
+      "author": "Ali Campillo",
       "title": "Mi perfecta Celia 😌🫶🏼",
       "preview": "Muchisimas felicidades a nuestra mami!!!",
       "body": "Mi perfecta Celia 😌🫶🏼\nMuchisimas felicidades a nuestra mami!!! Porque eres la mejor persona a la que puedo recurrir para contarle mis cosas y que, de campamento, amo nuestra conexión. Me encanta poder tener la confianza contigo de contarte practicamente todo y sobre todo cosas \"de adultas\" porque eres un referente para mí. Tus consejos y forma de ver la vida me han ayudado mucho para mejorar mis relaciones de amistad y de pareja. Es muy valioso lo que ofreces y estoy súper contenta de tenerte como apoyo. \nSiento que como animadora me puedes ayudar muchisimo, y tu influencia en mí sé que es la mejor, porque te admiro mucho. \nTu forma de ser siento que se parece a la mia y por eso congeniamos tan bien, porque sé que además de vivir conflictos parecidos, ten…"
@@ -316,7 +316,7 @@ window.CELIA_DATA = {
     {
       "id": "campa-verano",
       "symbol": "💛",
-      "author": "",
+      "author": "Mele",
       "title": "¡Feliz cumpleee, Celia!",
       "preview": "Me pareces una persona que transmite muchísima luz y alegría a quienes te rodean.",
       "body": "¡Feliz cumpleee, Celia! \n\nEspero que tengas un día precioso y que lo disfrutes muchísimo, porque te lo mereces.\n\nMe alegro un montón de poder verte y compartir contigo este campa de verano. Me pareces una persona que transmite muchísima luz y alegría a quienes te rodean. La verdad es que me encanta cuando participas en las actividades o te apuntas a cualquier reu o actividad, porque tienes una energía muy bonita que contagia. Consigues que me sienta muy cómoda y feliz.\n\nOjalá este nuevo año de vida venga cargado de momentos preciosos, de muchas alegrías y de personas que te hagan tan feliz como tú haces sentir a los demás. Y espero también que todas esas cosas bonitas te ayuden a afrontar y superar los momentos más difíciles.\n\nUn besote enorme. Te quiero muchito. 💛"
@@ -324,7 +324,7 @@ window.CELIA_DATA = {
     {
       "id": "seguir-conociendote",
       "symbol": "🎉",
-      "author": "",
+      "author": "Joaquín",
       "title": "Feliz cumpleaños, Celia! 🎉",
       "preview": "seguir conociéndote me parece una de las cosas más bonitas.",
       "body": "Feliz cumpleaños, Celia! 🎉\n\nEspero que pases un día genial y te traiga muchas cosas buenas. Aunque nos conocemos desde hace poco, me pareces increíble, seguir conociéndote me parece una de las cosas más bonitas. Disfruta mucho de tu día, y que no te falten motivos para sonreír. ¡Feliz cumple! 🥳"
@@ -332,7 +332,7 @@ window.CELIA_DATA = {
     {
       "id": "contigo-mejor",
       "symbol": "⛺",
-      "author": "",
+      "author": "Manu",
       "title": "contigo siempre es mejor todo.",
       "preview": "Hay personas que dejan huella sin darse cuenta, y tú eres una de ellas.",
       "body": "Hola Celia, muuuuuchas felicidades!! Este año va a ser el primero en el que no estemos en el mismo campamento y te echo mucho de menos, haces que todo sea más fácil y divertido, contigo siempre es mejor todo.\n\nEstá siendo un poco difícil y estamos más apartados, pero quiero que sepas que eso no cambia lo muchísimo que te aprecio. Siempre has sido una persona muy importante para mí y espero que nunca dejes de formar parte de mi vida. Hay personas que dejan huella sin darse cuenta, y tú eres una de ellas. Gracias por cada conversación, por cada risa, por cada momento compartido y por estar siempre cuando hacía falta.\n\nOjalá estés disfrutando un montón de tu día y de ese campa. Te deseo todo lo bueno que te mereces,  por ser como eres y por todo lo que aportas …"
@@ -340,7 +340,7 @@ window.CELIA_DATA = {
     {
       "id": "oscar",
       "symbol": "☀",
-      "author": "Oscar",
+      "author": "Óscar",
       "title": "Muchiiisimas felicidades Celia.",
       "preview": "Eres una persona extraordinaria, de una calidad humana inconmensurable",
       "body": "Muchiiisimas felicidades Celia. Eres una persona extraordinaria, de una calidad humana inconmensurable y un corazón tan auténtico como sincero. Mantén tu sonrisa siempre contigo. La energía tan positiva que nos regalas a todos tan solo con compartir tiempo contigo es un don que alegre la vida. Sigue siendo tú y un BESAZO….\n\n\nOscar"
@@ -348,7 +348,7 @@ window.CELIA_DATA = {
     {
       "id": "motivos",
       "symbol": "🌸",
-      "author": "",
+      "author": "Fran",
       "title": "que nunca te falten motivos para sonreír",
       "preview": "que sigas creciendo y alcanzando tus metas 🌸🌸.",
       "body": "Feliz cumpleaños, que seas muy feliz, que nunca te falten motivos para sonreír, que sigas creciendo y alcanzando tus metas 🌸🌸. Y cuando lleguen los días difíciles, que nunca te falten la fuerza y las personas que te recuerden lo valiosa que eres."
@@ -356,7 +356,7 @@ window.CELIA_DATA = {
     {
       "id": "chiqui",
       "symbol": "🎂",
-      "author": "",
+      "author": "Inés",
       "title": "¡Muchísimas felicidades, Celia! 🥳",
       "preview": "¡Te mereces todo lo bueno de este mundo!",
       "body": "¡Muchísimas felicidades, Celia! 🥳\nDisfruta mucho de estos 30 añitos y de todos los que están por venir. ¡Te mereces todo lo bueno de este mundo! Espero que este año estén llenos de muchas risas, salud y personas que te hagan tan feliz como tú nos haces a nosotros. \n\nSé que ahora estás a tope con el campa, así que a por todas con ello jejejejeje. Seguro que es un día chulísimo. Y la próxima vez que nos veamos, ¡lo celebramos también! \n\nUn abrazo enorme, chiqui. Y de nuevo, feliz cumpleaños. ✨🫶🏻🎂"
@@ -372,7 +372,7 @@ window.CELIA_DATA = {
     {
       "id": "preciosa-amiga",
       "symbol": "🏠",
-      "author": "",
+      "author": "Lucía",
       "title": "Mi preciosa amiga",
       "preview": "porque eres casa, esa persona a la que volver cuando crees que no puedes más.",
       "body": "Mi preciosa amiga, mil gracias por ser exactamente como eres, por cuidar de todos a todas horas, por hacernos sentir familia desde siempre y para siempre. Te quiero preciosa y te querré siempre, porque eres casa, esa persona a la que volver cuando crees que no puedes más. \n\nEsa persona con la que ser libre cuando el mundo no deja ser, gracias gracias y gracias por cada día y cada momento.\n\nMuchísimas felicidades mi pequeña, eres y serás siempre un ejemplo de todo lo que está bien en el mundo. Te quiero"
@@ -380,7 +380,7 @@ window.CELIA_DATA = {
     {
       "id": "companero-kraal",
       "symbol": "⚜",
-      "author": "Tú compañero de kraal.",
+      "author": "Gabri",
       "title": "Querida Celia,",
       "preview": "Me has enseñado lo que significa el cuidado, el cariño y la constancia.",
       "body": "Querida Celia,\nHaber compartido estos años contigo me hace muy feliz, eres un ejemplo constante, y me haces aprender siempre. Me has enseñado lo que significa el cuidado, el cariño y la constancia.\nMás allá de lo que hemos compartido como compañeros de kraal y como amigos eres una persona muy importante para mí. Eres fundamental en mi vida y tenerte cerca sabiendo que siempre puedo contar contigo me hace sentir genial.\nTú compañero de kraal."
@@ -388,7 +388,7 @@ window.CELIA_DATA = {
     {
       "id": "pedazo-animadora",
       "symbol": "💚",
-      "author": "",
+      "author": "Nicole",
       "title": "Felicidadeess Celiaaa.",
       "preview": "Eres una persona super dulce, eres todo amor",
       "body": "Felicidadeess Celiaaa. Quiero que sepas que eres una pedazo de animadora, no tenia ninguna duda de que ibamos a formar un gran clan contigo y con Gabri, pero despues de estos dos años creo que he visto q realmente no me equivocaba. Eres una persona super dulce, eres todo amor y tienes la capacidad de que cualquiera se sienta super querido a tu lado. Me encanta que te abras con nosotros siempre y nos permitas entrar poco a poco un poquillo más en tu vida y formar parte de ella.\n\nGracias por escucharnos cuando lo hemos necesitado, gracias por hacer que todos nos sintamos incluidos, por hacernos reir, por apoyarnos, por estar a nuestro lado cuando no estabamos bien, por facilitarnos las cosas y sobre todo gracias por ser no solo una animadora sino nuestra amiga…"
